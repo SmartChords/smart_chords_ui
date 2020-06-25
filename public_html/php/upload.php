fd572416dc7045bc509120d837309409f30e63ae
groovestation31785 <angroover@gmail.com>
2020-06-24 22:27:33 -0700

    Set up upload page to use flash messaging with no file upload, wrong file type, and small image errors

--- public_html/php/upload.php
+++ public_html/php/upload.php
@@ -1,34 +1,49 @@
 <?php
-$target_dir = "../img/uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-// $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-// var_dump($target_file);
-var_dump($_POST);
-var_dump($imageFileType);
-$imageInfo = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-$allowed = array('image/jpeg', 'image/png', 'image/jpg');
-if(isset($_POST["submit"])) {
-  if (!in_array($imageInfo["mime"], $allowed)) {
-      echo 'error - NOT CORRECT FILE TYPE';
-  } else {
-    $height = $imageInfo[0];
-    $width = $imageInfo[1];
-  // Check if image file is a actual image or fake image
-    var_dump($imageInfo);
-    if($width > 400 && $height > 500) {
-      header('Location: ../preview.php');
-      exit();
-      echo "Image is large enough!!";
-      $uploadOk = 1;
+require("../../resources/FlashMessages.php");
+if (!session_id()) session_start();
+$msg = new \Plasticbrain\FlashMessages\FlashMessages();
+
+if (isset($_POST["submit"])) {
+  if (!empty($_FILES["fileToUpload"]["name"])) {
+    $target_dir = "../img/uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    // var_dump($_FILES["fileToUpload"]);
+    // var_dump($_POST);
+    // var_dump($imageFileType);
+    $imageInfo = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $allowed = array('image/jpeg', 'image/png', 'image/jpg');
+    
+    if (!in_array($imageInfo["mime"], $allowed)) {
+        $msg->error("Please submit an image with a PNG or JPG file type");
+        header('Location: ../index.php');
+        exit();
     } else {
-      header('Location: ../index.php');
-      exit();
-      echo "image too small";
-      $uploadOk = 0;
+      $height = $imageInfo[0];
+      $width = $imageInfo[1];
+    // Check if image file is a actual image or fake image
+      // var_dump($imageInfo);
+      if($width > 400 && $height > 500) {
+        header('Location: ../preview.php');
+        exit();
+        echo "Image is large enough!!";
+        $uploadOk = 1;
+      } else {
+        $msg->error('Please submit an image that is at least size 400x500');
+        header('Location: ../index.php');
+        exit();
+        $uploadOk = 0;
+      }
     }
+  } else {
+    $msg->error('Please upload a file to continue!');
+    header('Location: ../index.php');
+    exit();
   }
 } else {
-  echo "SOMETHING FOR REALZ IS WRONG";
+  $msg->error('System error. Please contact the development team for assistance');
+  header('Location: ../index.php');
+  exit();
 }
 ?>
